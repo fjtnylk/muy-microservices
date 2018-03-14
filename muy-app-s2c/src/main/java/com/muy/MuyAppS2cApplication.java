5d881a132c1a690e3c73cab477a2be57be6a725f
yanglikai <yanglikai@yonghui.cn>
2018-03-14 16:00:09 +0800

    #yanglikai #2018-03-14 #1.muy-app-s2c项目追加

---
+++ muy-app-s2c/src/main/java/com/muy/MuyAppS2cApplication.java
@@ -0,0 +1,12 @@
+package com.muy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyAppS2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyAppS2cApplication.class, args);
+	}
+}
