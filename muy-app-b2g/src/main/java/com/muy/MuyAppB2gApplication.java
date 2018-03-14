e4ae39040aafee1a45da27e69d5424fe24465c89
yanglikai <yanglikai@yonghui.cn>
2018-03-14 15:31:22 +0800

    #yanglikai #2018-03-14 #1.muy-app-b2g项目追加

---
+++ muy-app-b2g/src/main/java/com/muy/MuyAppB2gApplication.java
@@ -0,0 +1,12 @@
+package com.muy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyAppB2gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyAppB2gApplication.class, args);
+	}
+}
