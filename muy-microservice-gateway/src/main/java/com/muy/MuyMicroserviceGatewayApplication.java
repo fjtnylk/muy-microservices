be7cac20f34e7a022da68ac6126cdfc16ad35572
yanglikai <yanglikai@yonghui.cn>
2018-03-14 15:56:24 +0800

    #yanglikai #2018-03-14 #1.muy-microservice-user-auth-center项目追加 #2.muy-microservice-gateway项目追加

---
+++ muy-microservice-gateway/src/main/java/com/muy/MuyMicroserviceGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.muy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyMicroserviceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyMicroserviceGatewayApplication.class, args);
+	}
+}
---
+++ muy-microservice-user-auth-center/src/main/java/com/muy/MuyMicroserviceUserAuthCenterApplication.java
@@ -0,0 +1,12 @@
+package com.muy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyMicroserviceUserAuthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyMicroserviceUserAuthCenterApplication.class, args);
+	}
+}
